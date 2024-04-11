a088c0dd4fab558b7d7a34370f41ff81ce47f2f6
Dat Hoang <fiezt@outlook.com>
2024-04-11 16:17:34 +0700

    added toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter as FontSans } from 'next/font/google'
 import './globals.css'
 
 import { cn } from '@/lib/utils'
+import { Toaster } from '@/components/ui/toaster'
 
 const fontSans = FontSans({
     subsets: ['latin'],
@@ -31,6 +32,7 @@ export default function RootLayout({
                 )}
             >
                 {children}
+                <Toaster />
             </body>
         </html>
     )
