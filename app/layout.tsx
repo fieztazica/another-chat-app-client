bec73dae7afc449a07979e6f185a7baa9cac07c8
Dat Hoang <fiezt@outlook.com>
2024-04-11 11:06:08 +0700

    init commit

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from 'next'
+import { Inter as FontSans } from 'next/font/google'
+import './globals.css'
+
+import { cn } from '@/lib/utils'
+
+const fontSans = FontSans({
+    subsets: ['latin'],
+    variable: '--font-sans',
+})
+
+export const metadata: Metadata = {
+    title: 'ACA',
+    description: 'Another chat app - MERN Stack',
+}
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode
+}>) {
+    return (
+        <html lang="en">
+            <body
+                className={cn(
+                    'min-h-screen bg-background font-sans antialiased',
+                    fontSans.variable
+                )}
+            >
+                {children}
+            </body>
+        </html>
+    )
+}
---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: `${process.env.BACKEND_URL}/:path*`,
+            },
+        ]
+    },
+}
+
+export default nextConfig
